bdc07b8b34ee122b6913ff62fea3c52a251ed66c
Souvik Halder <83994461+Souvik-Halder@users.noreply.github.com>
2023-05-06 12:55:57 +0530

    Update index.js

--- frontend/src/https/index.js
+++ frontend/src/https/index.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-const REACT_APP_API_URL="http://localhost:4000/api/v1"
+const REACT_APP_API_URL="https://jobmate-app-api.onrender.com/api/v1"
 const api = axios.create({
     baseURL:REACT_APP_API_URL,
     withCredentials: true,
