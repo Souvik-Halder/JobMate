6bf895d7f298b9d183cc59dbb308094566853a69
Souvik Halder <83994461+Souvik-Halder@users.noreply.github.com>
2023-05-06 14:01:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,9 @@ app.use(
       name: 'session',
       keys: ['profile', 'email'],
       maxAge: 24 * 60 * 60 * 100,
-      secret:true
+      secret:true,
+       httpOnly:true,
+      sameSite:'none',
     })
   );
   
